5126302e18f8e409e74957d58e0e707920df7233
ElreyInhumano <mqgareth@gmail.com>
2025-10-30 13:34:21 -0500

    player attack animations
    Matías Quintanilla

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using Unity.Cinemachine;
+public class PlayerAnimations : MonoBehaviour
+{
+    Animator anim;
+    float timer;
+    bool startTime;
+    public int attack;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Attack();
+        if (startTime)
+        {
+            Timer();
+        }
+    }
+
+    void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (timer >= 1.5)
+            {
+                attack = 1;
+                anim.SetInteger("Attack", attack);
+                timer = 0;
+            }
+            else
+            {
+                startTime = true;
+                attack++;
+                anim.SetInteger("Attack", attack);
+            }
+            
+            
+
+            if (attack >= 2)
+            {
+                attack = 0;
+                timer = 0;
+                startTime = false;
+            }
+        }
+        if (timer >= 2)
+        {
+            startTime = false;
+            attack = 0;
+            anim.SetInteger("Attack", attack);
+            timer = 0;
+        }
+    }
+
+    void Timer()
+    {
+        timer += Time.deltaTime;
+    }
+    void Shake()
+    {
+
+    }
+}
